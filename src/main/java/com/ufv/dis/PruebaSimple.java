9d3bbebc8c2ff804eda39262909cbd28b4992a1b
joansnb <joansnb@gmail.com>
2024-01-16 13:03:25 +0100

    Creando clase de prueba y creando su clase test

---
+++ src/main/java/com/ufv/dis/PruebaSimple.java
@@ -0,0 +1,13 @@
+package com.ufv.dis;
+
+public class PruebaSimple {
+    public boolean prueba1(int a, int b){
+
+        //devuelve true si a es mayor, falso caso contrario
+        if(a>b){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
