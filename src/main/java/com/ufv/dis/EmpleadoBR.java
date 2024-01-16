b9f7b14e277d520e220dcaef5267ecc96f1be9c5
joansnb <joansnb@gmail.com>
2024-01-16 13:39:41 +0100

    Agregando clases principales

---
+++ src/main/java/com/ufv/dis/EmpleadoBR.java
@@ -0,0 +1,11 @@
+package com.ufv.dis;
+
+public class EmpleadoBR {
+    double calculaSalarioBruto(String tipoEmpleado, double ventasMes, double horasExtra){
+        return  0;
+    }
+
+    double calculaSalarioNeto(double salarioBruto){
+        return 0;
+    }
+}
