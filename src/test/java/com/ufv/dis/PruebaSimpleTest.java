537f70a62d15978bde9993ac5db125c52554995d
joansnb <joansnb@gmail.com>
2024-01-16 13:10:58 +0100

    Terminando primera prueba

--- src/test/java/com/ufv/dis/PruebaSimpleTest.java
+++ src/test/java/com/ufv/dis/PruebaSimpleTest.java
@@ -6,7 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PruebaSimpleTest {
 
+    PruebaSimple miPrueba = new PruebaSimple();
     @Test
     void prueba1() {
+        assertEquals(false, miPrueba.prueba1(1,2));
     }
 }
